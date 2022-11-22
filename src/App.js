c95d006500a719e7eb24021bd9f329d02fe7df7b
dayanecode <dayanecode@gmail.com>
2022-11-21 22:56:16 -0300

    inserted dark model in the onClick method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './styles.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  
+  const [posts, setPosts] = useState([]);
+  const [theme, setTheme] = useState("day");
+
+
+  useEffect(() => fetchPosts(), []);
+
+  useEffect(() => console.log(posts), [posts]);
+
+  
+  function fetchPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        data.lenght = 10;
+        setPosts(data);
+      });
+  }
+
+  function handleTheme () {
+    setTheme((theme) => (theme === "night" ? "day" : "night"));
+
+  }
+
+ return (
+    <div className="main-container" data-theme={theme}>
+      <header>
+      <h1>BLOG DA DAY</h1>
+          <button className='btn primary'>Novo Post</button>
+          <button className='btn primary' onClick={() => handleTheme()}>
+            {theme === "night" ? "Day" : "Night"} Mode</button>
       </header>
+      <main>
+        {posts.map((post) => (
+       
+        <section className='post-card' 
+        id={post.id}
+        onClick={(e) => console.log(e)}
+        >
+          <div className='post-preview-content'>           
+            <div className='post-author' >
+              <img 
+                src='https://avatars.dicebear.com/api/adventurer/your-custom-seed.svg' 
+                alt='Avatar do autor'
+                className='author-avatar'                 
+              /> 
+              <p>DayRamos</p>
+            </div>
+            <div className='post-body'> 
+              <p className='post-title'>{post.title}</p>
+              <p className='post-preview'>{post.body}</p>          
+            </div>
+          </div>
+          <div className='post-img'>
+            <img 
+            src={`https://picsum.photos/id/${10 * post.id}/100`} 
+            alt='imagem do post' 
+            />
+          </div>
+        </section>
+        ))}
+        
+      </main>
+
+
+      <section className='fullscreen-post'>
+        <button className='btn second'>Fechar</button>
+          <h1> Título do Post</h1>
+          <p> Conteúdo do Post</p>
+        </section>
+
+
     </div>
-  );
+  )
 }
 
 export default App;
